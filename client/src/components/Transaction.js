89f8682ad0b4190637c4291bf0279fdc4872db85
Kalpit Shah <kalpit.fxdatalabs@gmail.com>
2020-04-06 12:58:47 +0530

    Added the backed api for this project + model + routes + controller + mongoose

---
+++ client/src/components/Transaction.js
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
+export const Transaction = props => {
+  const { deleteTransaction } = useContext(GlobalContext);
+  const sign = props.transaction.amount < 0 ? "-" : "+";
+  return (
+    <div>
+      <li className={props.transaction.amount < 0 ? "minus" : "plus"}>
+        {props.transaction.text}{" "}
+        <span>
+          {sign}${Math.abs(props.transaction.amount)}
+        </span>
+        <button
+          onClick={() => deleteTransaction(props.transaction._id)}
+          className="delete-btn"
+        >
+          x
+        </button>
+      </li>
+    </div>
+  );
+};
